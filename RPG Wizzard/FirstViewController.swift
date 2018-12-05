7354db00e9a145efc1bd55c8bea259ce25bdff51
Aleksander Bernat <wujas@me.com>
2018-12-05 22:29:17 +0100

    Initial Commit

---
+++ RPG Wizzard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RPG Wizzard
+//
+//  Created by Aleksander Bernat on 05.12.2018.
+//  Copyright © 2018 Aleksander Bernat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
